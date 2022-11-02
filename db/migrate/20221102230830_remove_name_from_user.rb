041f38a7d90f4ef763835f071c8220f37d157ed7
onifernando1 <onifernando1@gmail.com>
2022-11-02 23:09:28 +0000

    Remove first and last name replace with full name

---
+++ db/migrate/20221102230830_remove_name_from_user.rb
@@ -0,0 +1,7 @@
+class RemoveNameFromUser < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :first_name, :string
+    remove_column :users, :last_name, :string
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_224348) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_230830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,8 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_224348) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.date "birthday"
     t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
