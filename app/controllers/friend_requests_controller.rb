99da9dcf646b419c2619596bb024895b2ecdbcd3
onifernando1 <onifernando1@gmail.com>
2022-11-04 17:29:34 +0000

    Friendrequest index page and controller

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,5 +1,11 @@
 class FriendRequestsController < ApplicationController
 
+    
+    def index
+        @friend_requests = FriendRequest.all 
+    end 
+    
+    
     def new 
        @friend_request = FriendRequest.new()
     end 
