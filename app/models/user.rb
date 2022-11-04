13e39b29ec9b70e9268b700577ec4b388f32c2f9
onifernando1 <onifernando1@gmail.com>
2022-11-04 14:48:01 +0000

    add placeholder in sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+        
          validates :full_name, presence: true
          validates :birthday, presence: true
          validates :email, presence: true, uniqueness:true
