358c22a832bd0b0c55857793df5d8e1adac9422c
onifernando1 <onifernando1@gmail.com>
2022-11-06 17:00:58 +0000

    add Like model

---
+++ db/migrate/20221106165905_remove_post_like_table.rb
@@ -0,0 +1,5 @@
+class RemovePostLikeTable < ActiveRecord::Migration[7.0]
+  def remove_table
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_222443) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_170007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_222443) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+  end
+
+  create_table "post_likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
