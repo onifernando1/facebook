16af736087a6a9225a2e3d0ef21e36f1ce0b8f90
onifernando1 <onifernando1@gmail.com>
2022-11-07 23:57:17 +0000

    likes controller new create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+
+    def new
+        @like = Like.new() 
+    end 
+
+    def create 
+        @like = Like.new() 
+        if @like.save
+            redirect_to root_path
+        else 
+            redirect_to root_path, status: :unprocessable_entity
+        end 
+    end 
+
 end
