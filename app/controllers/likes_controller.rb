701948d14b805760cb0f4ad6ee035e0a56895de7
onifernando1 <onifernando1@gmail.com>
2022-11-07 23:59:36 +0000

    likes controller params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     end 
 
     def create 
-        @like = Like.new() 
+        @like = Like.new(like_params) 
         if @like.save
             redirect_to root_path
         else 
@@ -13,4 +13,9 @@ class LikesController < ApplicationController
         end 
     end 
 
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
 end
