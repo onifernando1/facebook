3fff0cffe3e2f8a2605aa0a78a7a911dd3271e15
onifernando1 <onifernando1@gmail.com>
2022-11-08 14:45:17 +0000

    comment model updated with columns

---
+++ db/migrate/20221108144348_add_columns_to_comments.rb
@@ -0,0 +1,8 @@
+class AddColumnsToComments < ActiveRecord::Migration[7.0]
+  def change
+
+      add_column :comments, :commenter_id, :integer
+      add_column :comments, :post_author_id, :integer
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_220005) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_144348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "commenter_id"
+    t.integer "post_author_id"
   end
 
   create_table "friend_requests", force: :cascade do |t|
