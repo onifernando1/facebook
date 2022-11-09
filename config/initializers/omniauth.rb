c691c5f6345fd85cfea6cb940bd81e42036740fa
onifernando1 <onifernando1@gmail.com>
2022-11-09 16:24:24 +0000

    try reverse oauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  
+end
+
+OmniAuth.config.allowed_request_methods = %i[get]
